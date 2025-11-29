c25261640ad364514a53c6ccbe1368185fa4c7dc
atharavabh-beep <atharavabh@gmail.com>
2025-11-29 21:40:36 +0530

    Add files via upload

---
+++ data types.py
@@ -0,0 +1,16 @@
+x=10
+
+a=5
+print("type of a:" ,type(a) )
+
+b=2.5
+print("type of b:" ,type(b) )
+
+c="atharava bhardwaj"
+print("type of c:" ,type(c) )
+
+d=False
+print("type of d:" ,type(d) )
+
+f=True
+print("type of f:" ,type(f) )
---
+++ string contagination.py
@@ -0,0 +1,11 @@
+print("concentrate the strings")
+print("enter the first strings: ")
+strOne=input()
+
+print("enter the second strings: ")
+strTwo=input()
+
+#concantenate the two strings
+#using adddition operation
+strThree = strOne +strTwo
+print("\nconcantenated string: ", strThree)
